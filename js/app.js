7c005f76750e82e53eeadf27c98e500d5393d19e
prestigecvisual <prestigecvisual@gmail.com>
2026-04-04 16:33:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Sistema iniciado");
+function calcular() {
+  const largura = parseFloat(document.getElementById("largura").value);
+  const altura = parseFloat(document.getElementById("altura").value);
+  const qtd = parseInt(document.getElementById("quantidade").value);
+
+  const area = largura * altura;
+  const areaTotal = area * qtd;
+
+  const precoChapa = sistema.configuracao.precoChapa;
+  const areaChapa = sistema.configuracao.larguraChapa * sistema.configuracao.alturaChapa;
+
+  const custoCm2 = precoChapa / areaChapa;
+  const custoTotal = areaTotal * custoCm2;
+
+  document.getElementById("resultado").innerHTML =
+    `Área total: ${areaTotal} cm² <br>
+     Custo estimado: R$ ${custoTotal.toFixed(2)}`;
+}
