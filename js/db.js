a7f2b685a5e2ed927f1790863c6e2cf9d5dbd0cc
prestigecvisual <prestigecvisual@gmail.com>
2026-04-05 15:13:38 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -6,12 +6,15 @@
 const PRODUTOS_PADRAO = [
     { nome: "Adesivo Simples", preco: 120.00, tipo: "m2" },
     { nome: "Adesivo Premium", preco: 150.00, tipo: "m2" },
+    { nome: "ACM Adesivado", preco: 280.00, tipo: "m2" },
+    { nome: "ACM Corte com Dobra", preco: 350.00, tipo: "m2" },
     { nome: "Placa PVC 2mm", preco: 150.00, tipo: "m2" },
     { nome: "Acrílico 3mm", preco: 350.00, tipo: "m2" },
     { nome: "Chaveiro 5x9 cm Acrílico 3mm", preco: 35.00, tipo: "unid" },
     { nome: "Chaveiro 6x10 cm Acrílico 3mm", preco: 35.00, tipo: "unid" },
     { nome: "Chaveiro 5,5x11 cm Acrílico 3mm", preco: 35.00, tipo: "unid" },
-    { nome: "Lona 440g", preco: 125.00, tipo: "m2" },
+    { nome: "Lona sem Acabamento 440g", preco: 125.00, tipo: "m2" },
+    { nome: "Lona com Acabamento 440g", preco: 145.00, tipo: "m2" },
     { nome: "Banner c/ Bastão", preco: 125.00, tipo: "unid" },
     { nome: "Cavalete Madeira", preco: 180.00, tipo: "unid" }
 ];
